c2c40c7985de2b2717b1f98520cdd1239cc71f09
Eric Wing <ewing -dot- public |-at-| gmail.com>
2012-09-26 12:20:32 -0700

    radar://12258849 NSTextField focus ring breaks with setFrameCenterRotation:

---
+++ RotateViewText/RotateViewOld/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  RotateViewOld
+//
+//  Created by Eric Wing on 9/7/12.
+//  Copyright (c) 2012 test. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+@property (assign) IBOutlet NSView *someView;
+@property (assign) IBOutlet NSTextField *textField;
+@property (assign) IBOutlet NSTextView *textView;
+@property (assign) IBOutlet NSScrollView *scrollView;
+
+@end
