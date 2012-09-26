20934af9cd31a2d011403cebe4382d1bda0ec881
Eric Wing <ewing -dot- public |-at-| gmail.com>
2012-09-26 12:24:46 -0700

    radar://12258737 NSView animator setFrameCenterRotation looks bad (rotation not truly centered)

---
+++ RotateViewOld/RotateViewOld/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  RotateViewOld
+//
+//  Created by Eric Wing on 9/7/12.
+//  Copyright (c) 2012 test. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+	return NSApplicationMain(argc, (const char **)argv);
+}
