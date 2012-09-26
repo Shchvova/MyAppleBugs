a6bf381b75dd663734eff7fc32b2cbd5ca046834
Eric Wing <ewing -dot- public |-at-| gmail.com>
2012-09-26 12:13:57 -0700

    radar://12268871 NSTextView/NSTextField leaves white background with setFrameCenterRotation:

---
+++ RotateViewText2/RotateViewOld/AppDelegate.m
@@ -0,0 +1,31 @@
+//
+//  AppDelegate.m
+//  RotateViewOld
+//
+//  Created by Eric Wing on 9/7/12.
+//  Copyright (c) 2012 test. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+- (IBAction)rotate:(id)sender
+{
+	[[[self someView] animator] setFrameCenterRotation:[[self someView] frameCenterRotation] + 90.0];
+	[[[self textField] animator] setFrameCenterRotation:[[self textField] frameCenterRotation] + 90.0];
+//	[[[self textView] animator] setFrameCenterRotation:[[self textView] frameCenterRotation] + 90.0];
+	[[[self scrollView] animator] setFrameCenterRotation:[[self scrollView] frameCenterRotation] + 90.0];
+
+}
+@end
